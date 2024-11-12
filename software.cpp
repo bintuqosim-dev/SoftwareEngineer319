ee8ce48b9f5b9d9b69f5ecc52c3105d4b599372e
Bekruzjan_1 <behruzkurbonov245@gmail.com>
2024-11-12 22:18:22 +0500

    bid endian and little endian code

---
+++ software.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdint>
+
+bool is_little_endian() {
+    uint16_t x = 0x1;
+    return *((uint8_t*)&x) == 0x1;
+}
+
+int main() {
+    if (is_little_endian()) {
+        std::cout << "System is Little Endian" << std::endl;
+    } else {
+        std::cout << "System is Big Endian" << std::endl;
+    }
+    return 0;
+}
